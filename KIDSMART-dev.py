1da352de1e9b4c5af38da8fb4ff60af85eafb9fd
hychua <85439265+hychua@users.noreply.github.com>
2021-06-24 16:44:55 +0800

    Add files via upload

--- KIDSMART-dev.py
+++ KIDSMART-dev.py
@@ -8,7 +8,6 @@ Created on Sat Jun 19 22:08:51 2021
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output, State
 import base64
 import pandas as pd
