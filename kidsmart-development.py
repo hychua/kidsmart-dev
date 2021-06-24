924c33eaac6196667d6616493262cff545916fb7
hychua <85439265+hychua@users.noreply.github.com>
2021-06-24 18:34:18 +0800

    Add files via upload

--- kidsmart-development.py
+++ kidsmart-development.py
@@ -82,7 +82,7 @@ def product_div(image, name, price, view):
                                    'color':'white',
                                    'backgroundColor':'green',
                                    'borderRadius':5,
-                                   'height':50,'width':150,
+                                   'height':75,'width':175,
                                    'font-family':'calibri', 'display':'inline-block',
                                    'margin':10}),
                 html.Button(
@@ -106,7 +106,6 @@ def product_div(image, name, price, view):
 header_page = html.Div([
     
     html.Div([
-                        html.Br(),
                         
                         html.Div([
                             html.Br(),
@@ -220,14 +219,6 @@ app.layout = html.Div([
                 html.P('168 Shopping Mall, 918 Soler St, Binondo, Manila, 1006 Metro Manila'),
                     #html.Iframe(src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d61776.1681570088!2d120.9600329777667!3d14.598476951293357!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397ca0ec391ad93%3A0x510fd5abc4fc32b8!2s168%20Shopping%20Mall!5e0!3m2!1sen!2sph!4v1624431949045!5m2!1sen!2sph", 
                     #    ),
-                    
-                
-            ]),
-            
-            
-                  ],
-                     ),
-            
             html.Div([
                 html.H3('Contact Us:'),
                 html.P('8123-4567'),
@@ -237,10 +228,15 @@ app.layout = html.Div([
             html.Div(
                 html.Img(src='data:image/png;base64,{}'.format(encoded_image.decode()),
                          style={'height':125}),
-                 ),
-    
+                 ),        
+                
+            ]),
+
+                  ],
+                     style={'text-align':'center'}),
+  
     ],
-    style={'text-align':'center'})
+    style={})
     
     ],
     )
