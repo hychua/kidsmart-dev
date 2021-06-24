1e7c93d3cfda95bc644ff0068c6f283c846166ff
hychua <85439265+hychua@users.noreply.github.com>
2021-06-24 18:08:14 +0800

    Add files via upload

--- kidsmart-development.py
+++ kidsmart-development.py
@@ -106,46 +106,19 @@ header_page = html.Div([
     html.Div([
                         html.Br(),
                         
-                        html.Div([html.Button(
-                        id='help-button',
-                        n_clicks=0,
-                        children='Help',
-                        style={'fontSize':18,
-                               'color':'rgb(255,123,0)',
-                               'backgroundColor':'rgb(255,255,255)',
-                               'borderRadius':5,
-                               'height':42,'width':100,
-                               'font-family':'minion', 'display':'none','float':'right',
-                               'margin-right':10}),
-                    
-                    html.Button(
-                        id='login-button',
-                        n_clicks=0,
-                        children='Login',
-                        style={'fontSize':18,
-                               'color':'rgb(255,123,0)',
-                               'backgroundColor':'rgb(255,255,255)',
-                               'borderRadius':5,
-                               'height':42,'width':100,
-                               'font-family':'minion', 'display':'none','float':'right',
-                               'margin-left':10}),
-                    ],
-                            style={'float':'right'}),
-                    
-
                         html.Div([
                             html.Br(),
-                            html.H1("KIDSMART Online Catalog",style={'fontSize':'300%','text-align':'center',
+                            html.H1("KIDSMART Online Catalog",style={'fontSize':'350%','text-align':'center',
                                                       'color':'rgb(255,255,255)','font-family':'calibri'}),
-                            html.Br(),
+
                             dcc.Input(id='search-bar', type = 'search',
                                          placeholder='Search for Products',
-                                         style={'fontSize':'150%','width':'75%',
-                                                'text-align':'center', 'borderRadius':5,}),
+                                         style={'fontSize':'275%','width':'80%',
+                                                'text-align':'center', 'borderRadius':20,}),
                                                   
                             
                             ],
-                                 style={'width': '75%', 'align-items': 'center',
+                                 style={'align-items': 'center',
                                         'justify-content': 'center'}),
                         html.Br(),
                     ],
@@ -235,27 +208,21 @@ app.layout = html.Div([
             
             html.Div([
                 html.H3('Where We Are:'),
-                html.Div([
-                    html.P('Tutuban Center Mall, C.M. Recto cor. R Antonio St., Binondo, Manila'),
+                
+                html.P('Tutuban Center Mall, C.M. Recto cor. R Antonio St., Binondo, Manila'),
                     #html.Iframe(src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3860.82211922851!2d120.97065481432026!3d14.609206880783512!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397ca0ce2555559%3A0x322c377e80b267e8!2sTutuban%20Shopping%20Mall!5e0!3m2!1sen!2sph!4v1624427880406!5m2!1sen!2sph",
                     #    ),
-                ],
-                         style={'display':'inline-block'}),
                 
-                html.Div([
-                    html.P('168 Shopping Mall, 918 Soler St, Binondo, Manila, 1006 Metro Manila'),
+                
+                
+                html.P('168 Shopping Mall, 918 Soler St, Binondo, Manila, 1006 Metro Manila'),
                     #html.Iframe(src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d61776.1681570088!2d120.9600329777667!3d14.598476951293357!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397ca0ec391ad93%3A0x510fd5abc4fc32b8!2s168%20Shopping%20Mall!5e0!3m2!1sen!2sph!4v1624431949045!5m2!1sen!2sph", 
                     #    ),
-                    ],
-                        style={'display':'inline-block','margin-left':25}),
+                    
                 
             ]),
             
-            html.Div(
-                html.Img(src='data:image/png;base64,{}'.format(encoded_image.decode()),
-                         style={'height':125,'float':'right','margin-right':20,
-                                'display':'inline-block'}),
-                 ),
+            
                   ],
                      ),
             
@@ -265,9 +232,13 @@ app.layout = html.Div([
                 html.P('example@gmail.com'),
             ],
                 ),
+            html.Div(
+                html.Img(src='data:image/png;base64,{}'.format(encoded_image.decode()),
+                         style={'height':125}),
+                 ),
     
     ],
-    style={'backgroundColor':'rgb(203,183,162)'})
+    style={'text-align':'center'})
     
     ],
     )
